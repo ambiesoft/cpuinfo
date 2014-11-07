f4dae7206e4aa93083f5b13bb99499e3d1e44d05
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2014-11-07 06:13:34 +0000

    start
    git-svn-id: svn://192.168.3.99/cpuinfo@4968 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ cpuinfo/cpuinfo.cpp
@@ -0,0 +1,61 @@
+// cpuinfo.cpp : main project file.
+
+#include "stdafx.h"
+#include "resource.h"
+
+#pragma comment(lib,"user32.lib")
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int mymain(array<System::String ^> ^args)
+{
+	// Enabling Windows XP visual effects before any controls are created
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	System::Text::StringBuilder sb;
+	sb.AppendLine(System::Environment::OSVersion->VersionString);
+
+	MEMORYSTATUSEX msx = {0};
+	msx.dwLength = sizeof(msx);
+	if(!GlobalMemoryStatusEx(&msx))
+	{
+		sb.AppendLine(gcnew String(_T("Function fails")));
+	}
+	else
+	{
+		TCHAR buff[256];
+		_stprintf(buff,
+			_T("%I64d MB of physical memory.\n"),
+            msx.ullTotalPhys/(1024*1024));
+		sb.AppendLine(gcnew String(buff));
+	}
+
+	HICON hIcon = ::LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON_MAIN));
+
+	NotifyIcon^ ni = gcnew NotifyIcon();
+	ni->BalloonTipText = sb.ToString();
+	ni->Icon = System::Drawing::Icon::FromHandle((IntPtr)hIcon);// gcnew System::Drawing::Icon(
+	//ni->Text = "aaa";
+	ni->Visible = true;
+	ni->ShowBalloonTip(5000);
+	::Sleep(5100);
+	delete ni;
+
+	return 0;
+}
+
+int main(array<System::String ^> ^args)
+{
+	try
+	{
+		return mymain(args);
+	}
+	catch(System::Exception^ ex)
+	{
+		MessageBox::Show(ex->Message);
+	}
+	return -1;
+}
---
+++ cpuinfo/stdafx.h
@@ -0,0 +1,14 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+#pragma once
+
+// TODO: reference additional headers your program requires here
+#include <checkaccess.h>
+#define _CRT_SECURE_NO_DEPRECATE
+#pragma warning(disable:4141 4996)
+
+#include <windows.h>
+#include <tchar.h>
+
+#include <vcclr.h>
